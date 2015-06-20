2fbbf0450f71456dc02dfa2cdf5ef652b9ca3946
Stephen Benner <sbenner9@gmail.com>
2015-06-20 14:45:09 -0700

    minor updates

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ActiveRecord::Base
   validates :title, presence: {message: 'Must have a title.'}
   validates :title, length: {minimum: 4, message: 'wtf too short'}
   # validates :title, format: {with: /[A-Z].+/}
-  validates :body, length: {maximum: 65536}
+  validates :title, length: {maximum: 32, message: '2 long'}
 
   def to_s
     title
